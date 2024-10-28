a6dab6f0702655ba8e4e9cbb34c8e4845e15fcbc
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-10-28 10:40:10 +0330

    Add NotFoundPage

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,17 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('src/layouts/mainLayout.vue'),
+    path: "/",
+    component: () => import("src/layouts/mainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/home/index.vue') },
-      { path: 'about', component: () => import('src/pages/aboutUs/aboutUs.vue') },
-      { path: 'contactUs', component: () => import('src/pages/contactUs/contactUs.vue') },
+      { path: "", component: () => import("src/pages/home/index.vue") },
+      {
+        path: "about",
+        component: () => import("src/pages/aboutUs/aboutUs.vue"),
+      },
+      {
+        path: "contactUs",
+        component: () => import("src/pages/contactUs/contactUs.vue"),
+      },
       // {
       //   path: 'InsuranceMagazine',
       //   component: () => import('src/pages/insuranceMagazine/insuranceMagazine.vue'),
@@ -17,80 +22,80 @@ const routes = [
       //   props: true
       // } ,
       {
-        path: 'trackingInsurance',
-        component: () => import('src/pages/trackingInsurance/trackingInsurance.vue'),
+        path: "trackingInsurance",
+        component: () =>
+          import("src/pages/trackingInsurance/trackingInsurance.vue"),
       },
       // {
       //   path: 'HomeBlogDetails',
       //   component: () => import('src/pages/homeBlogDetails/homeBlogDetails.vue'),
       // },
       {
-        path: 'trackingDamage',
-        component: () => import('src/pages/trackingDamage/trackingDamage.vue'),
+        path: "trackingDamage",
+        component: () => import("src/pages/trackingDamage/trackingDamage.vue"),
       },
       {
-        path: 'submitDamage',
-        component: () => import('src/pages/submitDamage/submitDamage.vue'),
+        path: "submitDamage",
+        component: () => import("src/pages/submitDamage/submitDamage.vue"),
       },
       {
-        path: 'marketerRegistration',
-        component: () => import('src/pages/marketerRegistration/marketerRegistration.vue'),
+        path: "marketerRegistration",
+        component: () =>
+          import("src/pages/marketerRegistration/marketerRegistration.vue"),
       },
       {
-        path: 'faq',
-        component: () => import('src/pages/faq/commonFaq.vue'),
+        path: "faq",
+        component: () => import("src/pages/faq/commonFaq.vue"),
       },
       {
-        path: 'ourService',
-        component: () => import('src/pages/ourService/ourService.vue'),
+        path: "ourService",
+        component: () => import("src/pages/ourService/ourService.vue"),
       },
       {
-        path: 'registerComplaint',
-        component: () => import('src/pages/registerComplaint/registerComplaint.vue'),
+        path: "registerComplaint",
+        component: () =>
+          import("src/pages/registerComplaint/registerComplaint.vue"),
       },
       {
-        path: 'otherJobsRegisteration',
-        component: () => import('src/pages/otherJobsRegisteration/otherJobsRegisteration.vue'),
+        path: "otherJobsRegisteration",
+        component: () =>
+          import("src/pages/otherJobsRegisteration/otherJobsRegisteration.vue"),
       },
       {
-        path: 'agentRegisteration',
-        component: () => import('src/pages/agentRegisteration/agentRegisteration.vue'),
+        path: "agentRegisteration",
+        component: () =>
+          import("src/pages/agentRegisteration/agentRegisteration.vue"),
       },
       {
-        path: 'blog',
-        component: () => import('src/pages/weblog/weblog.vue'),
+        path: "blog",
+        component: () => import("src/pages/weblog/weblog.vue"),
       },
       {
-        path: 'article/:id',
-        component: () => import('src/pages/weblog/article.vue'),
+        path: "article/:id",
+        component: () => import("src/pages/weblog/article.vue"),
       },
-    ]
+    ],
   },
- {
-
-    path: '/LoginPage',
-    component: () => import('src/pages/loginPage/loginPage.vue'),
-
- },
- {
-
-    path: '/RegisterPage',
-    component: () => import('src/pages/registerPage/registerPage.vue'),
-
- } ,
- ,
- {
-
-    path: '/forgetPassword',
-    component: () => import('src/pages/forgetPasswordPage/forgetPasswordPage.vue'),
-
- }
+  {
+    path: "/LoginPage",
+    component: () => import("src/pages/loginPage/loginPage.vue"),
+  },
+  {
+    path: "/RegisterPage",
+    component: () => import("src/pages/registerPage/registerPage.vue"),
+  },
+  ,
+  {
+    path: "/forgetPassword",
+    component: () =>
+      import("src/pages/forgetPasswordPage/forgetPasswordPage.vue"),
+  },
   // Always leave this as last one,
   // but you can also remove it
-  // {
-  //   path: '/:catchAll(.*)*',
-  //   component: () => import('pages/ErrorNotFound.vue')
-  // }
-]
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("src/pages/errorNotFound/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
