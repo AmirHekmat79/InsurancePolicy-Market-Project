904b8790da50dc6506c82cf69ed5792f718c9c51
majid <majid.abba3020@gmail.com>
2024-10-30 09:09:56 +0330

    Add version

--- public/js/config.js
+++ public/js/config.js
@@ -125,7 +125,7 @@ let Config = {
     }
   },
   getConfig() {
-    let config = this.setConfig(licensesName.Novin);
+    let config = this.setConfig(licensesName.Notifaano);
     return config;
   },
 };
